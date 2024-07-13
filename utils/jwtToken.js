dabc7e86693b932a3e17e693bb7a8a5a2e250b16
Paraspardhi98@gmail.com <paraspardhi98@gmail.com>
2024-07-13 21:25:03 +0530

    secure httpsonly

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -4,7 +4,9 @@ export const sendToken = (user, statusCode, res, message) => {
     expiresIn: new Date(
       Date.now() + 5 * 24 * 60 * 60 * 1000
     ),
-    // httpOnly: true, 
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Ensure the cookie is only sent over HTTPS in production
+    sameSite: "strict"
   };
 
   res.status(statusCode).cookie("token", token, options).json({
