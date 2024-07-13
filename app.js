637ed44f93df9f6d28b5bb6f97517615e340c621
Paraspardhi98@gmail.com <paraspardhi98@gmail.com>
2024-07-13 16:50:05 +0530

    add config.env file inside .gitignore

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename)
 
 const app = express();
-config({ path: "./config/config.env" });
+config({ path: "./config.env" });
 
 app.use(
   cors({
