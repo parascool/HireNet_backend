7f11f95eeefc380c614050954afc45960b065115
Paras-Pardhi <paraspardhi98@gmail.com>
2025-07-26 12:37:37 +0530

    change in all job API

---
+++ middlewares/auth.js
@@ -0,0 +1,19 @@
+import { User } from "../models/userSchema.js";
+import { catchAsyncErrors } from "./catchAsyncError.js";
+import ErrorHandler from "./error.js";
+import jwt from "jsonwebtoken";
+
+export const isAuthenticated = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+  if (!token) {
+    return next(new ErrorHandler("User is Not Authorized", 401));
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    req.user = await User.findById(decoded.id);
+    console.log('Authenticated user:', req.user);
+    next();
+  } catch (error) {
+    return next(new ErrorHandler("User Not Authorized", 402));
+  }
+});
---
+++ utils/jwtToken.js
@@ -0,0 +1,19 @@
+export const sendToken = (user, statusCode, res, message) => {
+  const token = user.getJWTToken();
+  const options = {
+    expiresIn: new Date(
+      Date.now() + 5 * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // Ensure the cookie is only sent over HTTPS in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax", // 'none' for cross-site cookies
+   
+  };
+
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    message,
+    token,
+  });
+};
